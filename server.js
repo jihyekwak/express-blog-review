e32a4ab63c20d8bca1ad9a352aabb5a473a34bb1
jihyekwak <jihyekwak53@gmail.com>
2022-01-10 01:06:38 -0800

    set up public folder and home routes

---
+++ server.js
@@ -0,0 +1,43 @@
+// External Modules
+const express = require("express");
+const methodOverride = require("method-override");
+
+// Internal Modules
+const routes = require("./routes");
+
+// Instanced Modules
+const app = express();
+
+// Configuration
+const PORT = 4000;
+
+app.set("view engine", "ejs");
+
+// Middleware
+// body data moddleware
+app.use(express.urlencoded({ extended: true}));
+// method override middleware
+app.use(methodOverride("_method"));
+// serve public files
+app.use(express.static("public"));
+// logger
+app.use((req, res, next) => {
+    console.log(req.url, req.method);
+    next();
+})
+
+// Routes & Controllers
+// Home routes
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to this blog app!</h1>")
+});
+
+// 404 Routes
+app.get((req, res) => {
+    res.send("404 Error! Page not found :(")
+});
+
+// Server Listener
+app.listen(PORT, () => {
+    console.log(`Blog app is live at http://localhost:${PORT}`);
+});
